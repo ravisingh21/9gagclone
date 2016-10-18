2b46f9aecebcc8c0f7c5858d90bec5e0ec633179
Rajender Thakur <rthakur.dev@gmail.com>
2016-10-18 11:43:10 +0530

    UI and php templating updated

--- php/index.php
+++ php/index.php
@@ -1,109 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Demo Project</title>
+<?php
+include('includes/header.php');
+include('includes/nav.php');
+?>
+<div class="container">
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet">
+  <!-- content-box -->
+  <div class="col-lg-8 content">
+        <div class="row">
+            <div class="col-lg-12">
+              <h4>When choosing a proper body text font, choose the ones that will suit your publication.</h4>
+            </div>
+            <div class="col-lg-12">
+                <img src="images/pexels-photo-26549.jpg">
+            </div>
+        </div>
+   </div>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-<body>
-<div class="dummy header">
-<nav class="navbar navbar-default navbar-inverse">
-<div class="container-fluid row">
 
-<!-- Brand and toggle get grouped for better mobile display -->
-<b>
-<div class="navbar-header pull-left">
-  <a class="navbar-brand" href="#"><img alt="icon" src="..."></a>
-  <a class="navbar-brand" href="#">Hot</a>
-  <a class="navbar-brand" href="#">Trending</a>
-  <a class="navbar-brand" href="#">Fresh</a>
-    <ul class="nav navbar-nav">
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sections
-          <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Funny</a></li>
-            <li><a href="#">WTF</a></li>
-            <li><a href="#">GIF</a></li>
-            <li><a href="#">NSFW</a></li>
-            <li><a href="#">Gaming</a></li>
-          </ul>
-        </li>
-      </ul></b>
-  <a class="navbar-brand" href="#">Video</a>
-  <a class="navbar-brand" href="#">Cosplay</a>
-  <a class="navbar-brand" href="#">Girl</a>
-  <a class="navbar-brand" href="#">NSFW</a>
-  <a class="navbar-brand" href="#">GIF</a>
-  <a class="navbar-brand" href="#">WTF</a>
-  <a class="navbar-brand" href="#">ASK</a>
-  <a class="navbar-brand" href="#">Movie</a>
-  <a class="navbar-brand" href="#">Anime</a>
-  <a class="navbar-brand" href="#">Gaming</a>
-  <a class="navbar-brand" href="#">Get Our App!</a>
-</div><!-- left header elements closed-->
-<div class="pull-right">
-  <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a>
-  <a class="navbar-brand" href="#">Log in</a>
-  <a class="navbar-brand" href="#"><button type="button" class="btn btn-xs btn-primary">Sign Up</button></a>
-</div><!-- right header elements closed-->
- </div>
-</nav><!--navigation bar closed-->
-</div><!--header closed-->
-<!-- Collect the nav links, forms, and other content for toggling -->
-<!-- /.container-fluid -->
-
-<!-- content-box -->
-
-<div class="col-lg-6 content">
-<div class="col-lg-12 col">
-<div class="col-lg-12">
-  <a class="Content-Heading" href="#">
-    <h4>When choosing a proper body text font, choose the ones that will suit your publication.</h4>
-  </a>
-<div class="col-lg-12">
-  <a class="content-image" href="">
-    <img src="images/pexels-photo-26549.jpg">
-  </a>
-</div>
-<div class="col-lg-12">
-  <a  class="hover" href="#">8,411 points</a>-<a class="hover" href="#">231 comments</a>
-<div class="row">
-  <div class="col-lg-6">  <button type="button" class="btn btn-default" aria-label="upload">
-    <span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></button>
-    <button type="button" class="btn btn-default" aria-label="download">
-      <span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></button>
-        <button type="button" class="btn btn-default" aria-label="comment">
-          <span class="glyphicon glyphicon-comment" aria-hidden="true"></span></button>
+ <!--ads--->
+  <div class="col-lg-4">
+    <div class="col-lg-12"><a class="ad-image" href=""><img src="images/pexels-photo-26549.jpg"></a></div>
   </div>
-    <div class=" col-lg-6">
-      <div class="pull-right"> <button type="button" class="btn btn-sm btn-primary">
-        <span class="glyphicon glyphicon-send" aria-hidden="true"> Blog</button>
-          <button type="button" class="btn btn-sm btn-info">
-            <span class="glyphicon glyphicon-send" aria-hidden="true"> Facebook</button>
-      </div>
-    </div>
-</div>
-</div>
-</div>
-</div>
-</div>
-<div class="col-lg-4 ad">
-  <div class="col-lg-12"><a class="ad-image" href=""><img src="images/pexels-photo-26549.jpg"></a></div>
+  <!--ads End--->
+
 </div>
 
-</body>
-</html>
+<?php
+include('includes/footer.php');
+?>
