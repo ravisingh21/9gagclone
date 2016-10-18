bf907598da5ea1d1524352a123d407ada4cae2cf
Rajender Thakur <rthakur.dev@gmail.com>
2016-10-18 11:52:20 +0530

    UI updated

--- php/index.php
+++ php/index.php
@@ -5,21 +5,19 @@ include('includes/nav.php');
 <div class="container">
 
   <!-- content-box -->
-  <div class="col-lg-8 content">
+  <div class="col-lg-8 content-container">
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-8 col-lg-offset-2 content">
               <h4>When choosing a proper body text font, choose the ones that will suit your publication.</h4>
+              <img src="images/pexels-photo-26549.jpg">
             </div>
-            <div class="col-lg-12">
-                <img src="images/pexels-photo-26549.jpg">
-            </div>
-        </div>
+          </div>
    </div>
 
 
  <!--ads--->
-  <div class="col-lg-4">
-    <div class="col-lg-12"><a class="ad-image" href=""><img src="images/pexels-photo-26549.jpg"></a></div>
+  <div class="col-lg-4 ads-container">
+    <div class="col-lg-12 content"><a class="ad-image" href=""><img src="images/pexels-photo-26549.jpg"></a></div>
   </div>
   <!--ads End--->
 
